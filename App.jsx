0d018f0d1882cf189457065c1063e055c5ebddff
sahilverma3120 <62200238+sahilverma3120@users.noreply.github.com>
2020-12-04 19:57:42 +0530

    digital clock
    Digital cock

--- App.jsx
+++ App.jsx
@@ -1,17 +1,17 @@
 import React, { useState } from 'react';
 const App = () => {
-  let newTime = new Date().toLocaleTimeString();
- const[ctime,sectime] = useState(newTime);
-
- const UpdateTime = () =>{
-  let newCTime = new Date().toLocaleTimeString();
-sectime(newCTime);
-}
- return(
+  let time = new Date().toLocaleTimeString();
+  const [ctime,setCtime] = useState(time);
+  const UpdateTime = () => {
+    time = new Date().toLocaleTimeString();
+    setCtime(time);
+  };
+  setInterval(UpdateTime,1000)
+  return(
     <>
     <h1>{ctime}</h1>
-    <button onClick = {UpdateTime}>get time</button>
     </>
-  )
- }
+  );
+ };
 export default App;
+ 
