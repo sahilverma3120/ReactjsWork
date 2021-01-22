d025b398cec4b0bbaa28341001ddeeb0dcc9675e
sahilverma3120 <62200238+sahilverma3120@users.noreply.github.com>
2021-01-22 20:20:28 +0530

    Create index.js

---
+++ Tesla-Homepage./src/index.js
@@ -0,0 +1,25 @@
+
+@font-face{
+  font-family: 'iCielMedium';
+  src: local('iCielMedium'), url('./fonts/iCiel-Medium.ttf') format('truetype');
+  font-weight: normal
+}
+*{
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+
+body {
+  margin: 0;
+  font-family: 'iCielMedium', -apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto', 'Oxygen',
+    'Ubuntu', 'Cantarell', 'Fira Sans', 'Droid Sans', 'Helvetica Neue',
+    sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+}
+
+code {
+  font-family: source-code-pro, Menlo, Monaco, Consolas, 'Courier New',
+    monospace;
+}
