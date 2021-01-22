546ec2f14bca31e3136daef4f35a3c0835dc4130
sahilverma3120 <62200238+sahilverma3120@users.noreply.github.com>
2021-01-22 20:21:23 +0530

    Create App.js

---
+++ Tesla-Homepage./src/App.js
@@ -0,0 +1,100 @@
+import './App.css';
+import Header from './components/Header';
+import Accessories from './assets/Desktop-Accessories.jpg';
+import ModelS from './assets/Desktop-ModelS.jpeg';
+import Model3 from './assets/Desktop-Model3.jpeg';
+import ModelX from './assets/Desktop-ModelX.jpeg';
+import ModelY from './assets/Desktop-ModelY.jpeg';
+import SolarPanels from './assets/Desktop-SolarPanels.jpeg';
+import SolarRoof from './assets/Desktop-SolarRoof.jpeg';
+import Item from './components/Item';
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <div className="app__itemsContainer">
+        <Item
+          title='Lowest Cost Solar Panels in America'
+          desc='Money-back guarantee'
+          desclink=''
+          backgroundImg={SolarPanels}
+          leftBtnTxt='ORDER NOW'
+          leftBtnLink=''
+          rightBtnTxt='LEARN MORE'
+          rightBtnLink=''
+          twoButtons='true'
+          first
+        />
+        <Item
+          title='Model S'
+          desc='$69,420'
+          descLink=''
+          backgroundImg={ModelS}
+          leftBtnTxt='CUSTOM ORDER'
+          leftBtnLink=''
+          rightBtnTxt='LEARN MORE'
+          rightBtnLink=''
+          twoButtons='true'
+        />
+        <Item
+          title='Model 3'
+          desc='Money-back guarantee'
+          descLink=''
+          backgroundImg={Model3}
+          leftBtnTxt='CUSTOM ORDER'
+          leftBtnLink=''
+          rightBtnTxt='LEARN MORE'
+          rightBtnLink=''
+          twoButtons='true'
+        />
+        <Item
+          title='Model X'
+          desc='Money-back guarantee'
+          descLink=''
+          backgroundImg={ModelX}
+          leftBtnTxt='CUSTOM ORDER'
+          leftBtnLink=''
+          rightBtnTxt='LEARN MORE'
+          rightBtnLink=''
+          twoButtons='true'
+        />
+        <Item
+          title='Model Y'
+          desc='Money-back guarantee'
+          descLink=''
+          backgroundImg={ModelY}
+          leftBtnTxt='CUSTOM ORDER'
+          leftBtnLink=''
+          rightBtnTxt='LEARN MORE'
+          rightBtnLink=''
+          twoButtons='true'
+        />
+        <Item
+          title='Solar for New Roofs'
+          desc='Money-back guarantee'
+          descLink=''
+          backgroundImg={SolarRoof}
+          leftBtnTxt='ORDER NOW'
+          leftBtnLink=''
+          rightBtnTxt='LEARN MORE'
+          rightBtnLink=''
+          twoButtons='true'
+        />
+        <Item
+          title='Accessories'
+          desc='Money-back guarantee'
+          descLink=''
+          backgroundImg={Accessories}
+          leftBtnTxt='SHOP NOW'
+          leftBtnLink=''
+          rightBtnTxt='LEARN MORE'
+          rightBtnLink=''
+          twoButtons='true'
+        />
+      </div>
+    </div>
+  );
+}
+
+export default App
